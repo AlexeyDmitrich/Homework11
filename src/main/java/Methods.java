0ac836e45657f38ac14a064720dc74b76d371fcf
alexey <a.d.kamenev@yandex.ru>
2022-06-20 21:35:14 +0300

    Homework11 v.0.1 созданы методы переноса и преобразования; добавлен абстрактный класс фрукт добавлены классы яблок и апельсинов добавлен класс коробок

---
+++ src/main/java/Methods.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Methods {
+    /**
+     * метод для смены местами элементов в массиве
+     * @param array массив
+     * @param inx1 индекс первого элемента
+     * @param inx2 индекс второго элемента
+     * @param <E> универсальный объектный тип
+     */
+    public <E> void replace (E[] array, int inx1, int inx2){
+        E element = array[inx1];
+        array[inx1] = array[inx2];
+        array[inx2] = element;
+    }
+
+    /**
+     * метод для преобразования массива в коллекцию
+     * @param array массив
+     * @param <T> универсальный тип данных
+     */
+    public <T> void arrToArrList (T[] array){
+        ArrayList <T> list = new ArrayList<>();
+        for (T element: array) {
+            list.add(element);
+        }
+    }
+}
